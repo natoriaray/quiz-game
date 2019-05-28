53b96758ded1ec79f7c6d07772a44108f5d7a7d5
natoriaray <natoriaray@gmail.com>
2019-05-28 15:19:07 -0500

    started creating function to find the checked answer

--- script.js
+++ script.js
@@ -1,25 +1,53 @@
 // DATA CONTROLLER
 var dataController = (function() {
+	var userAnswers, correctAnswersLeo;
 
-	//some code
+	correctAnswersLeo = []
+	userAnswers = [];
+
+	return {
+
+	}
 
 })();
 
 // UI CONTROLLER 
 var UIController = (function() {
+	var questionsLeo, questionsLeoArr;
 
-//some code
+	questionsLeo = {
+		1: document.getElementById('1'),
+		2: document.getElementById('2'),
+		3: document.getElementById('3'),
+		4: document.getElementById('4')
+	}
+
+	questionsLeoArr = [document.getElementById('1'), document.getElementById('2'), document.getElementById('3'), document.getElementById('4')];
+	return {
+		getInput: function(question, arr) {
+			if (question === 'question-1')
+			arr.forEach(function(el) {
+				if (el.checked) {
+					return el.id
+				};
+			});
+			}
+	}
 
 })();
 
-var contoller = (function() {
+var contoller = (function(dataCtrl, UICtrl) {
 
 	var eventListeners = function() {
 		document.querySelector('.nextBtn').addEventListener('click', nextQuestion);
 	};
 
 	var nextQuestion = function() {
-		console.log('event listeners working')
+		// 1. Get checked input
+		UICtrl.getInput();
+		// 2. Store answer in data structure
+
+		// 2. Change to the next question
 	}
 
 	return{
@@ -30,4 +58,4 @@ var contoller = (function() {
 
 })();
 
-contoller.init();
+contoller.init(dataController, UIController);
